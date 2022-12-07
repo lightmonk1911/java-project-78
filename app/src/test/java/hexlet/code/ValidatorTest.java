c9fe167d247afa1c01c30984b3ca7d71be6567b1
Igor Babenkov <igor.babenkov@corp.mail.ru>
2022-12-07 19:24:54 +0300

    fix: checkstyle fix

--- app/src/test/java/hexlet/code/ValidatorTest.java
+++ app/src/test/java/hexlet/code/ValidatorTest.java
@@ -1,10 +1,5 @@
 package hexlet.code;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
